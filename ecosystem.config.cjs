9cbdeb1537211046422a34480c08e2220005f755
yimyim <buildingamoon@gmail.com>
2025-02-04 10:12:52 +0800

    '20250204'

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,19 @@
 module.exports = {
-    apps: [
-      {
-        name: 'o-dotsfrontend',
-        port: '3000',
-        exec_mode: 'cluster',
-        instances: 'max',
-        script: './.output/server/index.mjs'
+  apps: [
+    {
+      name: 'o-dotsfrontend',
+      port: '3000',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs',
+      env: {
+        NODE_ENV: 'production',
+        BASE_URL: 'https://o-dots.com'
+      },
+      env_development: {
+        NODE_ENV: 'development',
+        BASE_URL: 'http://localhost:3000'
       }
-    ]
-  }
+    }
+  ]
+}
