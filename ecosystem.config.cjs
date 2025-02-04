5243330fe252292e2ae8abff210eef1a92e7191f
yimyim <buildingamoon@gmail.com>
2025-02-04 18:26:29 +0800

    'config'

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
       port: '3000',
       exec_mode: 'cluster',
       instances: 'max',
-      script: './.output/server/index.mjs',
+      script: './.output/server/index.js',
       env: {
         NODE_ENV: 'production',
         BASE_URL: 'https://o-dots.com'
