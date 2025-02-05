07fc37bb9a835d7ab094edb776b62caeb11b14cf
yimyim <buildingamoon@gmail.com>
2025-02-05 18:38:40 +0800

    'fake1'

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
       port: '3000',
       exec_mode: 'cluster',
       instances: 'max',
-      script: '/home/O-dots/frontend/.output/server/index.mjs',
+      script: './.output/server/index.mjs',
       env: {
         NODE_ENV: 'production',
         BASE_URL: 'https://o-dots.com'
