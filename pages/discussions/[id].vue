4439852fd81f8b3c3574ac24ca8e339edf663e5f
yimyim <buildingamoon@gmail.com>
2025-02-07 11:42:05 +0800

    'update'

--- pages/discussions/[id].vue
+++ pages/discussions/[id].vue
@@ -87,10 +87,7 @@ const messageContainer = ref(null);
 const runtimeConfig = useRuntimeConfig();
 const route = useRoute();
 const router = useRouter();
-const socket = io('https://backend.o-dots.com', {
-  transports: ['websocket', 'polling'], // Ensure fallback options are set
-  secure: true, // Ensure secure connection if using HTTPS
-});
+const socket = io('https://backend.o-dots.com');
 
 
 const discussionId = route.params.id;
