00a9d48dd5194d8d3ab1d08cf2714de36cf47ed2
yimyim <buildingamoon@gmail.com>
2025-02-13 17:40:24 +0800

    'update'

--- pages/payments/stripe.vue
+++ pages/payments/stripe.vue
@@ -5,38 +5,35 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import { useRuntimeConfig } from '#imports';
 import { loadStripe } from '@stripe/stripe-js';
+
 const runtimeConfig = useRuntimeConfig();
 
 const redirectToStripe = async () => {
   try {
-    const response = await fetch(runtimeConfig.public.apiBase + '/payments/create-checkout-session', {
+    const response = await fetch(`${runtimeConfig.public.apiBase}/payments/create-checkout-session`, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify({
-        productName: 'O-dots!',
+        productName: 'Some Name',
         price: 1000,
         quantity: 1,
-        successUrl: "https://o-dots/payments/success?session_id={CHECKOUT_SESSION_ID}",
-        failUrl: "https://https://o-dots/payments/fail",
-      })
+        successUrl: 'https://www.o-dots.com/payments/success?session_id={CHECKOUT_SESSION_ID}',
+        failUrl: 'https://www.o-dots.com/payments/fail',
+      }),
     });
 
     const data = await response.json();
-    console.log(data); // Log the response to check the session ID
-
-    //const stripe = await loadStripe(runtimeConfig.public.stripeTestKey);
-    const stripe = await loadStripe(runtimeConfig.public.stripePublishKey);
-    if (stripe) {
-      const { error } = await stripe.redirectToCheckout({ sessionId: data.id });
-      if (error) {
-        console.error("Stripe checkout error:", error);
-      }
-    } else {
-      console.error("Stripe initialization failed");
-    }
+    const stripe = await loadStripe(runtimeConfig.public.stripeTestKey);
+    //const stripe = await loadStripe(runtimeConfig.public.stripePublishKey);
+    await stripe.redirectToCheckout({ sessionId: data.id });
   } catch (error) {
-    console.error("Error during redirect to Stripe:", error);
+    console.error('Error during redirect to Stripe:', error);
+    alert('An unexpected error occurred. Please try again later.');
   }
 };
 </script>
