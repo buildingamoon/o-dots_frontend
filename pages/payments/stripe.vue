1e767931b691b9748aa45661d669e6b8dfa2f923
yimyim <buildingamoon@gmail.com>
2025-02-14 12:58:59 +0800

    'update'

--- pages/payments/stripe.vue
+++ pages/payments/stripe.vue
@@ -1,43 +1,51 @@
 <template>
   <div>
-    <button @click.prevent="redirectToStripe">Pay</button>
+      <button  @click.prevent="redirectToStripe" > Pay </button>
   </div>
 </template>
 
+
 <script setup>
-import { ref } from 'vue';
-import { useRuntimeConfig } from '#imports';
-import { loadStripe } from '@stripe/stripe-js';
+
+
+  import { loadStripe } from '@stripe/stripe-js';
+
+
+  const runtimeConfig = useRuntimeConfig();
 
 
 
-const runtimeConfig = useRuntimeConfig();
 
-const redirectToStripe = async () => {
-  try {
-    const response = await fetch(`${runtimeConfig.public.apiBase}/payments/create-checkout-session`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        productName: 'Some Name',
-        price: 1000,
-        quantity: 1,
-        successUrl: 'https://www.o-dots.com/payments/success?session_id={CHECKOUT_SESSION_ID}',
-        failUrl: 'https://www.o-dots.com/payments/fail',
-      }),
+  const redirectToStripe = async () => {
+
+
+
+
+
+
+    const response = await fetch(runtimeConfig.public.apiBase + '/payments/create-checkout-session', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    
+        body: JSON.stringify({
+
+
+          productName: 'Some Name',
+          price: 1000,
+          quantity: 1,
+          successUrl: "https://o-dots.com/payments/success?session_id={CHECKOUT_SESSION_ID}",
+          failUrl: "https://o-dots.com/payments/fail",
+   
+        })
     });
 
-
-
     const data = await response.json();
+   
     // const stripe = await loadStripe(runtimeConfig.public.stripeTestKey);
-   const stripe = await loadStripe(runtimeConfig.public.stripePublishKey);
+    const stripe = await loadStripe(runtimeConfig.public.stripePubishKey);
     await stripe.redirectToCheckout({ sessionId: data.id });
-  } catch (error) {
-    console.error('Error during redirect to Stripe:', error);
-    alert('An unexpected error occurred. Please try again later.');
+
   }
-};
 </script>
